75ade3506cb0e318e8cb3e539a9c9f4002169780
Aribazz <ariyoaribass68@gmail.com>
2022-07-21 15:21:51 +0800

    Changes to be committed:

---
+++ main.js
@@ -0,0 +1,89 @@
+/*var Name = prompt("what is your name!:");
+alert(Name + " ...You Welcome to the best website... ");
+let firstName = "Aribass"
+console.log(firstName);
+
+let myFirstJob = "Adviser";
+let myCurrentJob = "Programmer";
+
+console.log(myFirstJob);
+console.log(myFirstJob);
+console.log(Name);
+//it is for the description of data...
+let country = 2000000;
+let continent = 1000000;
+let population = 50000;
+
+console.log(country);
+console.log(typeof 23);
+console.log(typeof myCurrentJob);
+let numberCount = prompt("Enter your phone number: ");
+console.log(numberCount);
+
+
+//Assignment for pratices
+let isIsland = true;
+console.log(typeof isIsland);
+let language;
+console.log(typeof language);
+console.log(population);
+
+
+//mathematicall operators
+const current = 2022
+const ageAri = current - 1996;
+const ageJohn = current - 1998;
+console.log(ageAri, ageJohn);
+
+
+//assignment operators
+let x = 10 + 3;
+x = 12;
+x--; //x=x-1
+x += 10;
+x++;
+console.log(x);
+
+//comaprison assignment
+console.log(ageAri > ageJohn);
+console.log(ageAri >= 18);
+
+let number = 1;
+console.log(number);
+
+console.log(++number++);
+console.log(++number);
+
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+
+const markMass = 78;
+const markHeight = 1.69;
+const johnMass = 92;
+const johnHeight = 1.95;
+
+
+const markMass = 95;
+const markHeight = 1.88;
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const BMIMark = markMass / markHeight ** 2;
+const BMIJohn = johnMass / johnHeight ** 2;
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
+*/
+const myName = "Ariyo";
+const job = "programmer";
+const birthYear = 1996;
+
+console.log("I am " + myName + " a " + job + " by profession" + " born");
+
+
+
+//bm1 = mass / height ** 2; for the formulae purpose..
+//const markHigherBMI =
+    //console.log(mark, john);
